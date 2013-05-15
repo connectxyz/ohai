98767ba6d62039099167ed252d970176ba804e2f
root <root@52-54-0-ca-55-95.(none)>
2013-05-15 21:44:55 +0000

    wtf

--- files/default/plugins/changeDefaultIPInNodeData.rb
+++ files/default/plugins/changeDefaultIPInNodeData.rb
@@ -2,10 +2,6 @@ network['interfaces'].each do |iface, addrs|
 	if iface.eql?('eth0')
 		addrs['addresses'].each do |ip, params|	
 			self.ipaddress ip if params['family'].eql?('inet')
-			#network["ipaddress_#{iface}"] = ip if params['family'].eql?('inet')
-			#network["ipaddress6_#{iface}"] = ip if params['family'].eql?('inet6')
-			#network["macaddress_#{iface}"] = ip if params['family'].eql?('lladdr')
-			#realip = ip if params['family'].eql?('inet') and iface.eql?(network_interfaces['private'])
 		end
   	end
 end
